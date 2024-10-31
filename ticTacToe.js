58ffae51a1807bf9b1e0462719a7963e8af3ab80
amaypip481 <179745277+amaypip481@users.noreply.github.com>
2024-10-31 09:24:27 +0530

    Tic Tac Toe game

---
+++ ticTacToe.js
@@ -0,0 +1,90 @@
+let playerTurn = 0;
+let playerOneScore = [];
+let playerTwoScore = [];
+
+function initiategame() {
+    let buttons = document.querySelectorAll(".tictactoebuttons");
+
+    buttons.forEach((e) => { e.addEventListener("click", buttonpressed) });
+
+    let playAgainButtion = document.querySelector("#playAgainbutton");
+    playAgainButtion.addEventListener("click", playAgain);
+
+}
+
+function buttonpressed(e) {
+    if (e.target.textContent != "") return;
+    else if (playerTurn === 0) {
+        e.target.textContent = "O";
+        playerOneScore.push(e.target.id);
+        if (checkWinner(playerOneScore)) {
+            let winnnerDiv = document.querySelector("#winnerDiv");
+            winnnerDiv.style.display = "flex"
+            let winnerHeader = document.querySelector("#winnerDiveText");
+            winnerHeader.textContent = "Player 1 wins!"
+        }
+        playerTurn = 1;
+        let playerdetail = document.querySelector("#player1Header");
+        playerdetail.textContent = "Player 2 please select";
+    }
+
+    else if (playerTurn === 1) {
+        e.target.textContent = "X";
+        playerTwoScore.push(e.target.id);
+        if (checkWinner(playerTwoScore)) {
+            let winnnerDiv = document.querySelector("#winnerDiv");
+            winnnerDiv.style.display = "flex"
+            let winnerHeader = document.querySelector("#winnerDiveText");
+            winnerHeader.textContent = "Player 2 wins!"
+        }
+        playerTurn = 0;
+        let playerdetail = document.querySelector("#player1Header");
+        playerdetail.textContent = "Player 1 please select";
+    }
+}
+
+function checkWinner(playerScore) {
+
+    let mergedArray = playerOneScore + playerTwoScore;
+
+    if ((playerScore.includes("1") && playerScore.includes("2") && playerScore.includes("3")) ||
+        (playerScore.includes("1") && playerScore.includes("4") && playerScore.includes("7")) ||
+        (playerScore.includes("1") && playerScore.includes("5") && playerScore.includes("9")) ||
+        (playerScore.includes("4") && playerScore.includes("5") && playerScore.includes("6")) ||
+        (playerScore.includes("7") && playerScore.includes("8") && playerScore.includes("9")) ||
+        (playerScore.includes("2") && playerScore.includes("5") && playerScore.includes("8")) ||
+        (playerScore.includes("3") && playerScore.includes("6") && playerScore.includes("9")) ||
+        (playerScore.includes("3") && playerScore.includes("5") && playerScore.includes("7")))
+        return true;
+    
+    else if(mergedArray.includes("1") && mergedArray.includes("2") && mergedArray.includes("3") && 
+    mergedArray.includes("4") && mergedArray.includes("5") && mergedArray.includes("6") &&
+    mergedArray.includes("7") && mergedArray.includes("8") && mergedArray.includes("9"))
+    {
+        let winnnerDiv = document.querySelector("#winnerDiv");
+        winnnerDiv.style.display = "flex"
+        let winnerHeader = document.querySelector("#winnerDiveText");
+        winnerHeader.textContent = "Draw!"
+    }
+    else return false;
+
+}
+
+function playAgain() {
+    let winnnerDiv = document.querySelector("#winnerDiv");
+    winnnerDiv.style.display = "none"
+
+    let buttons = document.querySelectorAll(".tictactoebuttons");
+
+    buttons.forEach((e) => { e.textContent = "" });
+
+    let playerdetail = document.querySelector("#player1Header");
+    playerdetail.textContent = "Player 1 please select";
+
+    playerTurn = 0;
+    playerOneScore = [];
+    playerTwoScore = [];
+
+}
+
+initiategame();
